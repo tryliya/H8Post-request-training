51cf8bc3cc31087d3b99c9a72e5a0a59420827c0
tryliya <tryliya@gmail.com>
2021-07-16 16:22:35 +0300

    add solution

--- src/App.js
+++ src/App.js
@@ -1,3 +1,28 @@
+import React, { useState } from 'react';
+import axios from 'axios'
 
+
+const App = () => {
+  const  [user, setUser] = useState('')
+
+  const signIn = () => {
+    axios.post("http://localhost:3001/auth/sign-in", { "phone": "+19254223749", "password": "123" })
+    .then((response) => {
+      setUser(response.data.email)
+    })
+  }
+
+  return (
+    <>
+    <button onClick={signIn}>Sign In</button>
+    <div>{user}</div>
+    </>
+  )
+}
+
+
+
+
+export default App
 
 
